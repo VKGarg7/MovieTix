ee58be0281b9b1682652b70383745182961e5c2e
VKGarg7 <14garg04@gmail.com>
2025-07-13 11:49:50 +0530

    created funtions to create , delete and update user data

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,12 +5,12 @@ export const inngest = new Inngest({ id: "movie=ticket-booking" });
 
 //Inngest functionn to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk./users.created'},
-    async ({event}) => {
-        const {id , first_name, last_name, email_addresses , image_url} = event.data
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk./users.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
-            _id: id, 
+            _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
@@ -18,6 +18,31 @@ const syncUserCreation = inngest.createFunction(
         await User.create(userData)
     }
 )
+//Inngest functionn to delete user data to a database
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-from-clerk' },
+    { event: 'clerk./users.deleted' },
+    async ({ event }) => {
+        const { id } = event.data
+        await User.findByIdAndDelete(id)
+    }
+)
+//Inngest functionn to update user data to a database
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk./users.updated' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
 
 
-export const functions = [syncUserCreation];
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
