20334431e82e770da114cdc8cb1e631fd993cfba
VKGarg7 <14garg04@gmail.com>
2025-07-17 19:08:27 +0530

    change in the import file

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,50 +1,99 @@
+// // // import { Inngest } from "inngest";
+// // // import User from "../models/User.js";
+
+// // // // Create a client to send and receive events
+// // // export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// // // //Inngest functionn to save user data to a database
+// // // const syncUserCreation = inngest.createFunction(
+// // //     { id: 'sync-user-from-clerk' },
+// // //     { event: 'clerk/user.created' },
+// // //     async ({ event }) => {
+// // //         const { id, first_name, last_name, email_addresses, image_url } = event.data
+// // //         const userData = {
+// // //             _id: id,
+// // //             email: email_addresses[0].email_address,
+// // //             name: first_name + ' ' + last_name,
+// // //             image: image_url
+// // //         }
+// // //         await User.create(userData)
+// // //     }
+// // // )
+// // // //Inngest functionn to delete user data to a database
+// // // const syncUserDeletion = inngest.createFunction(
+// // //     { id: 'delete-user-from-clerk' },
+// // //     { event: 'clerk/user.deleted' },
+// // //     async ({ event }) => {
+// // //         const { id } = event.data
+// // //         await User.findByIdAndDelete(id)
+// // //     }
+// // // )
+// // // //Inngest functionn to update user data to a database
+// // // const syncUserUpdation = inngest.createFunction(
+// // //     { id: 'update-user-from-clerk' },
+// // //     { event: 'clerk/user.updated' },
+// // //     async ({ event }) => {
+// // //         const { id, first_name, last_name, email_addresses, image_url } = event.data
+// // //         const userData = {
+// // //             _id: id,
+// // //             email: email_addresses[0].email_address,
+// // //             name: first_name + ' ' + last_name,
+// // //             image: image_url
+// // //         }
+// // //         await User.findByIdAndUpdate(id, userData)
+// // //     }
+// // // )
+
+
+
+// // // export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+
+
 // // import { Inngest } from "inngest";
 // // import User from "../models/User.js";
 
-// // // Create a client to send and receive events
 // // export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-// // //Inngest functionn to save user data to a database
+// // // Create user
 // // const syncUserCreation = inngest.createFunction(
-// //     { id: 'sync-user-from-clerk' },
-// //     { event: 'clerk/user.created' },
-// //     async ({ event }) => {
-// //         const { id, first_name, last_name, email_addresses, image_url } = event.data
-// //         const userData = {
-// //             _id: id,
-// //             email: email_addresses[0].email_address,
-// //             name: first_name + ' ' + last_name,
-// //             image: image_url
-// //         }
-// //         await User.create(userData)
-// //     }
-// // )
-// // //Inngest functionn to delete user data to a database
+// //   { id: "sync-user-from-clerk" },
+// //   { event: "clerk.user.created" },
+// //   async ({ event }) => {
+// //     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+// //     const userData = {
+// //       _id: id,
+// //       email: email_addresses?.[0]?.email_address || "",
+// //       name: `${first_name} ${last_name}`,
+// //       image: image_url,
+// //     };
+// //     await User.create(userData);
+// //   }
+// // );
+
+// // // Delete user
 // // const syncUserDeletion = inngest.createFunction(
-// //     { id: 'delete-user-from-clerk' },
-// //     { event: 'clerk/user.deleted' },
-// //     async ({ event }) => {
-// //         const { id } = event.data
-// //         await User.findByIdAndDelete(id)
-// //     }
-// // )
-// // //Inngest functionn to update user data to a database
+// //   { id: "delete-user-from-clerk" },
+// //   { event: "clerk.user.deleted" },
+// //   async ({ event }) => {
+// //     const { id } = event.data;
+// //     await User.findByIdAndDelete(id);
+// //   }
+// // );
+
+// // // Update user
 // // const syncUserUpdation = inngest.createFunction(
-// //     { id: 'update-user-from-clerk' },
-// //     { event: 'clerk/user.updated' },
-// //     async ({ event }) => {
-// //         const { id, first_name, last_name, email_addresses, image_url } = event.data
-// //         const userData = {
-// //             _id: id,
-// //             email: email_addresses[0].email_address,
-// //             name: first_name + ' ' + last_name,
-// //             image: image_url
-// //         }
-// //         await User.findByIdAndUpdate(id, userData)
-// //     }
-// // )
-
-
+// //   { id: "update-user-from-clerk" },
+// //   { event: "clerk.user.updated" },
+// //   async ({ event }) => {
+// //     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+// //     const userData = {
+// //       email: email_addresses?.[0]?.email_address || "",
+// //       name: `${first_name} ${last_name}`,
+// //       image: image_url,
+// //     };
+// //     await User.findByIdAndUpdate(id, userData);
+// //   }
+// // );
 
 // // export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
 
@@ -66,7 +115,8 @@
 //       name: `${first_name} ${last_name}`,
 //       image: image_url,
 //     };
-//     await User.create(userData);
+//     await User.findByIdAndUpdate(id, userData, { upsert: true });
+//     console.log("User created or updated:", id);
 //   }
 // );
 
@@ -75,8 +125,13 @@
 //   { id: "delete-user-from-clerk" },
 //   { event: "clerk.user.deleted" },
 //   async ({ event }) => {
-//     const { id } = event.data;
+//     const id = event.data?.id || event.data?.object?.id;
+//     if (!id) {
+//       console.error("User ID missing in delete event");
+//       return;
+//     }
 //     await User.findByIdAndDelete(id);
+//     console.log("User deleted:", id);
 //   }
 // );
 
@@ -91,7 +146,8 @@
 //       name: `${first_name} ${last_name}`,
 //       image: image_url,
 //     };
-//     await User.findByIdAndUpdate(id, userData);
+//     await User.findByIdAndUpdate(id, userData, { upsert: true });
+//     console.log("User updated:", id);
 //   }
 // );
 
@@ -101,54 +157,74 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
 
+// Create Inngest client
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-// Create user
+/**
+ * User Creation Function
+ * Triggered on clerk.user.created
+ */
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk.user.created" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    
     const userData = {
       _id: id,
       email: email_addresses?.[0]?.email_address || "",
       name: `${first_name} ${last_name}`,
       image: image_url,
     };
+
     await User.findByIdAndUpdate(id, userData, { upsert: true });
-    console.log("User created or updated:", id);
+    return { status: "User created or updated", id };
   }
 );
 
-// Delete user
+/**
+ * User Deletion Function
+ * Triggered on clerk.user.deleted
+ */
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-from-clerk" },
   { event: "clerk.user.deleted" },
   async ({ event }) => {
     const id = event.data?.id || event.data?.object?.id;
+
     if (!id) {
-      console.error("User ID missing in delete event");
-      return;
+      return { error: "User ID not found in event data" };
     }
+
     await User.findByIdAndDelete(id);
-    console.log("User deleted:", id);
+    return { status: "User deleted", id };
   }
 );
 
-// Update user
+/**
+ * User Update Function
+ * Triggered on clerk.user.updated
+ */
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk.user.updated" },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    
     const userData = {
       email: email_addresses?.[0]?.email_address || "",
       name: `${first_name} ${last_name}`,
       image: image_url,
     };
+
     await User.findByIdAndUpdate(id, userData, { upsert: true });
-    console.log("User updated:", id);
+    return { status: "User updated", id };
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+// Export all functions for Inngest registration
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
