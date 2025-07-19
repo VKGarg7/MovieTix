608f72b0f5871d84c134a9fa24aeeac1af8851ee
VKGarg7 <14garg04@gmail.com>
2025-07-20 02:04:55 +0530

    getting the dashboard data from the backend

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -11,8 +11,13 @@ import Title from "../../components/admin/Title";
 import BlurCircle from "../../components/BlurCircle";
 import { UsersIcon } from "lucide-react";
 import { dateFormat } from "../../lib/dateFomat";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+
+  const {axios , getToken , user , image_base_url} = useAppContext();
+
   const currency = import.meta.env.VITE_CURRENCY;
 
   const [dashboardData, setDashboardData] = useState({
@@ -47,13 +52,28 @@ const Dashboard = () => {
   ];
 
   const fetchDashboardData = async () => {
-    setDashboardData(dummyDashboardData);
-    setLoading(false);
+    try {
+      const {data} = await axios.get("/api/admin/dashboard-data", {
+        headers: { Authorization: `Bearer ${await getToken()}`}})
+
+        if(data.success){
+          setDashboardData(data.dashboardData)
+          setLoading(false)
+        }else{
+          toast.error(data.message)
+        }
+ 
+    } catch (error) {
+      toast.error("Failed to fetch dashboard data");
+      console.error(error);  
+    }
   };
 
   useEffect(() => {
-    fetchDashboardData();
-  }, []);
+    if(user){
+      fetchDashboardData();
+    }
+  }, [user]);
 
   return !loading ? (
     <>
@@ -86,7 +106,7 @@ const Dashboard = () => {
         <BlurCircle top="-100px" left="-10%" />
         {dashboardData.activeShows.map((show)=>(
           <div key={show._id} className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300">
-            <img src={show.movie.poster_path} alt="" className="h-60 w-full object-cover"/>
+            <img src={image_base_url + show.movie.poster_path} alt="" className="h-60 w-full object-cover"/>
             <p className="font-medium p-2 truncate">{show.movie.title}</p>
 
             <div className="flex items-center justify-between px-2">
